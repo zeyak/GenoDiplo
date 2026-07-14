82811449e97ac47b0112a540196dcbad7b9676f8
Zeynep Akdeniz <34158306+zeyak@users.noreply.github.com>
2026-07-14 16:06:42 +0200

    Delete workflow directory

--- workflow/rules/scripts/Genomics/1_Assembly/2_Assemblers/FlyeAssembler.py
+++
@@ -1,8 +0,0 @@
-from snakemake.shell import shell
-
-input = snakemake.input
-out_dir = snakemake.output.out_dir
-genome_size = snakemake.params.genome_size
-threads = snakemake.params.threads
-
-shell(f"""flye --nano-hq {input} --genome-size {genome_size} --threads {threads} --out-dir {out_dir}""")
--- workflow/rules/scripts/Genomics/2_Annotation/1_Structural/ProdigalAnnotation.py
+++
@@ -1,8 +0,0 @@
-from snakemake.shell import shell
-
-assembly = snakemake.input.assembly
-
-gff = snakemake.output.gff
-faa = snakemake.output.faa
- 
-shell(f"prodigal -i {assembly} -o {gff} -g 6 -f gff -c -a {faa}""")
